5b070a690ce9786823a56c752599b998b70dc48d
SongHye <hungern95@gmail.com>
2016-04-22 11:31:05 +0900

    중간고사 기본 마크업 수정, js 초기

---
+++ mid/midjs.js
@@ -0,0 +1,7 @@
+var board_tab = document.getElementById('board');
+var video_tab = document.getElementById('video');
+var image_tab = document.getElementById('image');
+var tips_tab  = document.getElementById('tips');
+var blog_tab  = document.getElementById('blog');
+var cafe_tab  = document.getElementById('cafe');
+
