e9a5b953b5763e4b7909a8690c5460d8d0db5b4d
SongHye <hungern95@gmail.com>
2016-04-22 23:57:46 +0900

    더보기 버튼 수정 전까지 완료!!!!

--- 06/api과제/searchjs.js
+++ 06/api과제/searchjs.js
@@ -1,24 +1,49 @@
 console.log('Hello searchapi.js');
 var wrap = document.getElementById('wrap');
-var q = document.getElementById('searchkeyword')
-var apikey = '6aaaae9ac6df9b23cd7f506304ffe14f';
+var searchkeyword = document.getElementById('searchkeyword')
+var q;
 var template = document.getElementById('searchresult').innerHTML;
+var searchbtn = document.getElementById('searchbtn');
+var page;
 
+searchbtn.addEventListener('click', dosearch);
 
+function dosearch(){
+    q = searchkeyword.value;
+    page = 1;
+    getjson(makeUrl(q));
+}
 
-
-
+function getjson(apiurl) {
+    getJSON(apiurl, function(res) {console.log(res);
+                                   render(wrap, template, res.channel.item);
+                                   mksmbtn();
+                                 }
+           );
+}
 
 function makeUrl(q){
+    var apikey = '6aaaae9ac6df9b23cd7f506304ffe14f';
     var apiurl = 'https://apis.daum.net/search/vclip?q='+q+'&apikey='+apikey+'&output=json';
+    console.log('url 이 생성되었습니다.');
     return apiurl;
 }
 
-console.log(apiurl);
+function render(wrap, template, data) {
+    var html = tmpl(template, {list: data});
+    wrap.innerHTML = html;
+    console.log('render 함수 실행');
+}
 
-function render(wrap, template, data){
-    var html = tmpl(template, {list: channel.item});
+function mksmbtn(){
+    var addbutton = wrap.innerHTML;
+    addbutton += '<button onclick="viewmore()">더보기</button>'
     wrap.innerHTML = html;
 }
 
-getJSON(apiurl, function(res){ console.log(res); });
+function viewmore(){
+    if(page==3) {alert('더 이상 표시할 내용이 없습니다.');}
+    else{page++;
+         console.log('page 1 증가, page : ' + page);
+    }
+}
