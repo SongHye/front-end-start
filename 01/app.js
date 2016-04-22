1b3367acb3c7b082a02dce58c77629c9f92260f9
SongHye <hungern95@gmail.com>
2016-04-22 11:10:04 +0900

    01 add

---
+++ 01/app.js
@@ -0,0 +1,52 @@
+console.log("Hello World");
+
+//var str = 'hello world';
+//console.log(str);
+
+//var x = 'abc';
+//var cityname = 'jeju';
+//var cityName = 'jeju';
+//var nick = 'hulk';
+//var age = 30;
+//var hobby = 'bike';
+
+//var nick = 'my nick is \'songhye\'';
+//var homepage = "songhye.github.io";
+
+//var isMan = 'true';
+//console.log(nick=="");
+
+
+
+var man = {
+    name : 'songhye',
+    age : 22,
+    hobby : 'bowling'
+};
+
+//for (var count=0; count<5; count++){
+//    console.log(count);
+//}
+
+function sum(x) {
+    var y = 2;
+    return x + y;
+}
+//console.log();
+//var z = sum(10);
+console.log(sum(10));
+
+//alert("Loveu")
+
+
+/*if (man.name === "songhye") {
+    console.log("songhye");
+}
+    else { console.log("WHO?"); }
+
+
+if (man.age < 30) {
+    console.log("young");
+}
+    else { console.log("old"); }
+*/
---
+++ 01/app2.js
@@ -0,0 +1,40 @@
+var str = 'hello world';
+console.log(str);
+
+var x = 'abc';
+var cityname = 'jeju';
+var cityName = 'jeju';
+var nick = 'hulk';
+var age = 30;
+var hobby = 'bike';
+
+var nick = 'my nick is \'songhye\'';
+var homepage = "songhye.github.io";
+
+var isMan = 'true';
+console.log(nick=="");
+console.log(!!'test');
+
+var log = function(str){
+    
+    console.log(str);
+};
+
+function log(str){
+    console.log(str);
+}
+
+
+var image ={}
+
+image.width=1;
+image.height=2;
+
+image['width']=1; //연관배열
+image['name'+1]='1.jpg';  //동적으로도 할당 가능
+
+image = {width:1, height:2}
+
+
+
+
